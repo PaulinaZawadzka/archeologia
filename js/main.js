22af802115944ebba4e14d0e209a287268bea286
Paulina Zawadzka <p_zawadzka@op.pl>
2019-01-24 21:59:49 +0100

    change

--- js/main.js
+++ js/main.js
@@ -1,16 +1,3 @@
-jQuery(function ($) {
-	$('.table').footable();
-});
-
-
-jQuery(function ($) {
-	$('.table').footable({
-		"columns": $.get('columns.json'),
-		"rows": $.get('rows.json')
-	});
-});
-
-
 $(function () {
 
 	/* Efekt Smooth Scroll */
